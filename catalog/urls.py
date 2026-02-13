2447205e5a90a287bc1c73678d3d18a9d6808808
Василий <kulikov821999@gmail.com>
2026-02-13 17:15:52 +0300

    renamed main_page to home, fixes for urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home_view, name='main_page'),
+    path("", views.home_view, name='home'),
     path("contacts/", views.contacts_view, name='contacts'),
     path("category/", views.contacts_view, name='category'),
     path("catalogue/", views.contacts_view, name='catalogue'),
